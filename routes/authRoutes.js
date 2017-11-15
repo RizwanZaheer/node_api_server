3305f0c68bf1e39c16f1bac9d49b829ad5c8832b
RIZWAN\Rizwan <rizwanzaheer003@gmail.com>
2017-11-15 23:55:47 +0500

    adding signin/signup routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,20 +1,20 @@
-// const passport = require("passport");
-// const Authentication = require("../controllers/authentication");
-// const passportService = require("../services/passport");
+const passport = require("passport");
+const Authentication = require("../controllers/authentication");
+const passportService = require("../services/passport");
 
-// const requireAuth = passport.authenticate("jwt", { session: false });
-// const requireSignin = passport.authenticate("local", { session: false });
+const requireAuth = passport.authenticate("jwt", { session: false });
+const requireSignin = passport.authenticate("local", { session: false });
 
 // Routes
 module.exports = app => {
   app.get("/", (req, res) => {
     res.send({ message: "Super secret code is bala bala!" });
   });
-  // // Signin post Routes
-  // app.post("/signin", requireSignin, Authentication.signin);
+  // Signin post Routes
+  app.post("/signin", requireSignin, Authentication.signin);
 
-  // // Singup Post Route
-  // app.post("/signup", Authentication.signup);
+  // Singup Post Route
+  app.post("/signup", Authentication.signup);
 
   // app.get(
   //   "/auth/google/",
