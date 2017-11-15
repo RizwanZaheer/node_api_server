2c1a204ab3a89c7c3fae75cf8c543bb98ea22a34
RIZWAN\Rizwan <rizwanzaheer003@gmail.com>
2017-11-15 23:52:33 +0500

    disable routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,28 +16,28 @@ module.exports = app => {
   // // Singup Post Route
   // app.post("/signup", Authentication.signup);
 
-  app.get(
-    "/auth/google/",
-    passport.authenticate("google", {
-      scope: ["profile", "email"]
-    })
-  );
-  app.get(
-    "/auth/google/callback",
-    passport.authenticate("google"),
-    (req, res) => {
-      console.log("surveys");
-      res.redirect("/surveys");
-    }
-  );
+  // app.get(
+  //   "/auth/google/",
+  //   passport.authenticate("google", {
+  //     scope: ["profile", "email"]
+  //   })
+  // );
+  // app.get(
+  //   "/auth/google/callback",
+  //   passport.authenticate("google"),
+  //   (req, res) => {
+  //     console.log("surveys");
+  //     res.redirect("/surveys");
+  //   }
+  // );
 
-  app.get("/api/logout", (req, res) => {
-    req.logout();
-    res.send(req.user);
-  });
+  // app.get("/api/logout", (req, res) => {
+  //   req.logout();
+  //   res.send(req.user);
+  // });
 
-  app.get("/api/current_user", (req, res) => {
-    console.log("current_user");
-    res.send(req.user);
-  });
+  // app.get("/api/current_user", (req, res) => {
+  //   console.log("current_user");
+  //   res.send(req.user);
+  // });
 };
